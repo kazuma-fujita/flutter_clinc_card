5ce25198e1cff9ef52a5f5afcda315a33b8d0369
kazuma-fujita <kazuma.fujita.zuma@gmail.com>
2021-03-03 17:49:47 +0900

    Upsert clinic card added.

--- lib/nested_list_screen.dart
+++ lib/nested_list_screen.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_clinc_card/upsert_clinic_card.dart';
 import 'package:like_button/like_button.dart';
-
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'image_slider.dart';
+
+class Const {
+  static const routeNameUpsertClinicCard = '/upsert-clinic-card';
+}
 
 class NestedListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(primaryColor: Colors.white),
+      routes: <String, WidgetBuilder>{
+        Const.routeNameUpsertClinicCard: (BuildContext context) =>
+            UpsertClinicCard(),
+      },
       home: NestedList(),
     );
   }
@@ -18,6 +27,7 @@ class NestedList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        elevation: 0,
         title: const Text('Clinic Card List'),
       ),
       body: ListView.builder(
@@ -72,70 +82,90 @@ class NestedList extends StatelessWidget {
         // viewportFractionで隣同士の要素間隔割合を調整
         controller: PageController(viewportFraction: 0.9),
         itemBuilder: (context, horizontalIndex) =>
-            _buildHorizontalView(context, verticalIndex, horizontalIndex),
+            // _buildHorizontalView(context, verticalIndex, horizontalIndex),
+            ClinicCardWidget(),
       ),
     );
   }
+}
 
-  Widget _buildHorizontalView(
-      BuildContext context, int verticalIndex, int horizontalIndex) {
+class ClinicCardWidget extends HookWidget {
+  @override
+  Widget build(BuildContext context) {
+    final isPushed = useState<bool>(false);
     return Padding(
       // 隣同士の要素間隔を調整
       padding: const EdgeInsets.symmetric(horizontal: 4),
-      child: Card(
-        shape: RoundedRectangleBorder(
-          side: const BorderSide(color: Colors.black12, width: 1),
-          borderRadius: BorderRadius.circular(16),
-        ),
-        // clipBehavior: Clip.antiAliasWithSaveLayer,
-        elevation: 2,
-        // margin: EdgeInsets.zero,
-        child: Row(
-          children: [
-            const Spacer(),
-            Expanded(
-              flex: 2,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: const <Widget>[
-                  Expanded(
-                    flex: 1,
-                    child: Center(
-                      child: Text(
-                        '診察券',
-                        style: TextStyle(
-                          color: Colors.black,
+      child: AnimatedPadding(
+        duration: const Duration(milliseconds: 80),
+        padding: EdgeInsets.all(isPushed.value ? 16 : 0),
+        child: GestureDetector(
+          onTapDown: (TapDownDetails downDetails) {
+            isPushed.value = true;
+          },
+          onTap: () {
+            isPushed.value = false;
+            Navigator.pushNamed(context, Const.routeNameUpsertClinicCard);
+          },
+          onTapCancel: () {
+            isPushed.value = false;
+          },
+          child: Card(
+            shape: RoundedRectangleBorder(
+              side: const BorderSide(color: Colors.black12, width: 1),
+              borderRadius: BorderRadius.circular(16),
+            ),
+            // clipBehavior: Clip.antiAliasWithSaveLayer,
+            elevation: 2,
+            // margin: EdgeInsets.zero,
+            child: Row(
+              children: [
+                const Spacer(),
+                Expanded(
+                  flex: 2,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const <Widget>[
+                      Expanded(
+                        flex: 1,
+                        child: Center(
+                          child: Text(
+                            '診察券',
+                            style: TextStyle(
+                              color: Colors.black,
+                            ),
+                          ),
                         ),
                       ),
-                    ),
+                      Expanded(
+                        flex: 2,
+                        child: Text(
+                          '診察券番号',
+                          style: TextStyle(fontSize: 12, color: Colors.grey),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 2,
+                        child: Text(
+                          '次回予約日時メモ',
+                          style: TextStyle(fontSize: 12, color: Colors.grey),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 2,
+                        child: Text(
+                          '前回受付日時',
+                          style: TextStyle(fontSize: 12, color: Colors.grey),
+                        ),
+                      ),
+                    ],
                   ),
-                  Expanded(
-                    flex: 2,
-                    child: Text(
-                      '診察券番号',
-                      style: TextStyle(fontSize: 12, color: Colors.grey),
-                    ),
-                  ),
-                  Expanded(
-                    flex: 2,
-                    child: Text(
-                      '次回予約日時メモ',
-                      style: TextStyle(fontSize: 12, color: Colors.grey),
-                    ),
-                  ),
-                  Expanded(
-                    flex: 2,
-                    child: Text(
-                      '前回受付日時',
-                      style: TextStyle(fontSize: 12, color: Colors.grey),
-                    ),
-                  ),
-                ],
-              ),
+                ),
+                const Spacer(),
+              ],
             ),
-            const Spacer(),
-          ],
+          ),
         ),
       ),
     );
---
+++ lib/upsert_clinic_card.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class UpsertClinicCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('診察券'),
+      ),
+      body: ClinicCardForm(),
+    );
+  }
+}
+
+class ClinicCardForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 64),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            flex: 1,
+            child: Center(
+              child: TextField(
+                maxLength: 16,
+                maxLengthEnforced: true,
+                decoration: const InputDecoration(
+                  hintText: '診察券番号を入力してください',
+                  labelText: '診察券番号',
+                ),
+                onSubmitted: (String name) {
+                  Navigator.pop(context);
+                },
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Center(
+              child: TextField(
+                maxLength: 16,
+                maxLengthEnforced: true,
+                decoration: const InputDecoration(
+                  hintText: '次回予約日時メモを入力してください',
+                  labelText: '次回予約日時メモ',
+                ),
+                onSubmitted: (String name) {
+                  Navigator.pop(context);
+                },
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Center(
+              child: RaisedButton(
+                color: Theme.of(context).primaryColorLight,
+                child: const Text('登録する'),
+                onPressed: () => Navigator.pop(context),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
